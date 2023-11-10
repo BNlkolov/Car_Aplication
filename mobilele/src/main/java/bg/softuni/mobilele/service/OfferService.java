ccc342c5412aa54034bfae7c1a2e224377cc41cf
Borislav Nikolov <borislavn441@gmail.com>
2023-11-10 21:24:58 +0200

    Remove CurrentUser

--- mobilele/src/main/java/bg/softuni/mobilele/service/OfferService.java
+++ mobilele/src/main/java/bg/softuni/mobilele/service/OfferService.java
@@ -1,7 +1,6 @@
 package bg.softuni.mobilele.service;
 
 import bg.softuni.mobilele.model.dto.AddOfferDTO;
-import bg.softuni.mobilele.model.dto.BrandDTO;
 import bg.softuni.mobilele.model.entity.ModelEntity;
 import bg.softuni.mobilele.model.entity.OfferEntity;
 import bg.softuni.mobilele.model.entity.UserEntity;
@@ -9,10 +8,7 @@ import bg.softuni.mobilele.model.mapper.OfferMapper;
 import bg.softuni.mobilele.repository.ModelRepository;
 import bg.softuni.mobilele.repository.OfferRepository;
 import bg.softuni.mobilele.repository.UserRepository;
-import bg.softuni.mobilele.user.CurrentUser;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @Service
 public class OfferService {
--- mobilele/src/main/java/bg/softuni/mobilele/service/UserService.java
+++ mobilele/src/main/java/bg/softuni/mobilele/service/UserService.java
@@ -5,10 +5,8 @@ import bg.softuni.mobilele.model.dto.UserRegisterDTO;
 import bg.softuni.mobilele.model.entity.UserEntity;
 import bg.softuni.mobilele.model.mapper.UserMapper;
 import bg.softuni.mobilele.repository.UserRepository;
-import bg.softuni.mobilele.user.CurrentUser;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -16,19 +14,15 @@ import java.util.Optional;
 
 @Service
 public class UserService {
-    private Logger LOGGER = LoggerFactory.getLogger(UserService.class);
 
-    private UserRepository userRepository;
-    private CurrentUser currentUser;
-    private PasswordEncoder passwordEncoder;
-    private UserMapper userMapper;
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final UserMapper userMapper;
 
     public UserService(UserRepository userRepository,
-                       CurrentUser currentUser,
                        PasswordEncoder passwordEncoder,
                        UserMapper userMapper) {
         this.userRepository = userRepository;
-        this.currentUser = currentUser;
         this.passwordEncoder = passwordEncoder;
         this.userMapper = userMapper;
 
@@ -46,34 +40,9 @@ public class UserService {
     }
 
 
-    public boolean login(UserLoginDTO loginDTO) {
-        Optional<UserEntity> userOpt = userRepository.
-                findByEmail(loginDTO.getUsername());
 
-        if (userOpt.isEmpty()) {
-            LOGGER.debug("User with name [{}] not found.", loginDTO.getUsername());
-            return false;
-        }
-        String rawPassword = loginDTO.getPassword();
-        String encodedPassword = userOpt.get().getPassword();
-
-        boolean success = passwordEncoder.matches(rawPassword, encodedPassword);
-
-        if (success) {
-            login(userOpt.get());
-        } else {
-            logout();
-        }
-        return success;
-    }
 
     private void login(UserEntity userEntity) {
-        currentUser.setLoggedIn(true)
-                .setName(userEntity.getFirstName() + " " + userEntity.getLastName()).
-                setEmail(userEntity.getEmail());
-    }
-    public void logout() {
-        currentUser.clear();
-
+               //TODO
     }
 }
